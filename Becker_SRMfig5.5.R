37cd349bbbcad354def2e9bc41616424fe46aa2a
supmilk <supmilk@hotmail.com>
2015-11-03 00:19:11 +0100

    Create Becker_SRMfig5.5.R

---
+++ Becker_SRMfig5.5.R
@@ -0,0 +1,10 @@
+# figur5.5-becker
+
+# mit package QRM
+
+library(QRM)
+
+###
+
+plot(rcopula.clayton(500,3,2),pch=19,ann=F,cex=0.6,cex.axis=0.6)
+plot(rcopula.frank(500,6,2),pch=19,ann=F,cex=0.6,cex.axis=0.6)
